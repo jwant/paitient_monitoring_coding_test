b28690384dd28caf6b5694a365edd8be0e184121
jwant <jjgwant@gmail.com>
2022-02-19 08:55:37 +1100

    FEAT: create patients, new menu

--- paitient_monitoring_project/patient_measurements/urls.py
+++ paitient_monitoring_project/patient_measurements/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profiles', views.profiles, name='profiles'),
     path('create_clinician', views.create_clinician, name='create_clinician'),
+    path('create_patient', views.create_patient, name='create_patient'),
     path('', include('django.contrib.auth.urls')),  # new
 ]
--- paitient_monitoring_project/patient_measurements/views.py
+++ paitient_monitoring_project/patient_measurements/views.py
@@ -6,14 +6,26 @@ from .models import Profile
 
 
 def index(request):
+    return render(request, 'index.html')
+
+def profiles(request):
     clinician_ids = User.objects.filter(profile__is_clinician=True)
+    patient_ids = User.objects.filter(profile__is_patient=True)
     context = {
         'clinician_ids': clinician_ids,
+        'patient_ids': patient_ids,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'profiles.html', context)
 
 def create_clinician(request):
-    # user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    new_profile(request, 'clinician')
+    return profiles(request)
+
+def create_patient(request):
+    new_profile(request, 'patient')
+    return profiles(request)
+
+def new_profile(request, type):
     u = User.objects.create_user(
         first_name=request.POST.get('first_name', False),
         last_name=request.POST.get('last_name', False),
@@ -22,15 +34,10 @@ def create_clinician(request):
         password=request.POST.get('password', False),
     )
     u.save()
-    p = Profile.objects.create(
-        user_id=u.id,
-        is_clinician=True,
-    )
+
+    p = Profile.objects.create(user_id=u.id,)
+    if type=='patient':
+        p.is_patient = True
+    elif type=='clincian':
+        p.is_clinician = True
     p.save()
-
-    clinician_ids = User.objects.filter(profile__is_clinician=True)
-    context = {
-        'clinician_ids': clinician_ids,
-    }
-
-    return render(request, 'index.html',context)
